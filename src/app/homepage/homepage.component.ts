8d398a9c9e9e008f0520f796372ca7423805991f
jiten1990 <jiten@coronation.in>
2023-08-10 19:15:48 +0530

    Swiper fixed - Jiten

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -16,6 +16,77 @@ export class HomepageComponent implements OnInit {
     },
   };
 
+  public  homeProductSwiper : SwiperConfigInterface = {
+    spaceBetween: 40,
+    navigation: {
+        nextEl: ".swiper-button-next.swiper-button-next-homeproduct",
+        prevEl: ".swiper-button-prev.swiper-button-prev-homeproduct",
+    },
+  }
+
+  public clientsSwiper : SwiperConfigInterface = {
+    slidesPerView: 5.3,
+    spaceBetween: 120,
+    navigation: {
+        nextEl: ".swiper-button-next.swiper-button-next-clients",
+        prevEl: ".swiper-button-prev.swiper-button-prev-clients",
+    },
+    breakpoints: {
+        1920: {
+            slidesPerView: 5.3,
+            spaceBetween: 120,
+        },
+        1440: {
+            slidesPerView: 4.75,
+            spaceBetween: 120,
+        },
+        1366: {
+            slidesPerView: 4.75,
+            spaceBetween: 90,
+        },
+        1280: {
+            slidesPerView: 4.75,
+            spaceBetween: 75,
+        },
+        1024: {
+            slidesPerView: 4.75,
+            spaceBetween: 60,
+        },
+        768: {
+            slidesPerView: 4.25,
+            spaceBetween: 30,
+        },
+        640: {
+            slidesPerView: 2.25,
+            spaceBetween: 15,
+        },
+        428: {
+            slidesPerView: 2.25,
+            spaceBetween: 15,
+        },
+        430: {
+            slidesPerView: 2.25,
+            spaceBetween: 15,
+        },
+        390: {
+            slidesPerView: 2.25,
+            spaceBetween: 15,
+        },
+        375: {
+            slidesPerView: 2.25,
+            spaceBetween: 15,
+        },
+        360: {
+            slidesPerView: 2.25,
+            spaceBetween: 15,
+        },
+        650: {
+            slidesPerView: 2.25,
+            spaceBetween: 15,
+        },
+    },
+  }
+
   constructor(
     public router : Router
   ) { }
